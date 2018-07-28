a890e100cd95f5aeeb1817e11c12efed84c61782
rwinklewski <rene.winklewski@gmail.com>
2018-07-28 07:01:38 -0700

    - complete unit tests

--- tests/Mocks/SimpleTestHandler.php
+++ tests/Mocks/SimpleTestHandler.php
@@ -7,23 +7,40 @@ use MicroShard\JsonRpcServer\Request;
 
 class SimpleTestHandler implements HandlerInterface
 {
+    /**
+     * @var string
+     */
     public $testValue;
+
+    /**
+     * @var bool
+     */
+    public $throwException = false;
 
     /**
      * SimpleTestHandler constructor.
      * @param $testValue
      */
-    public function __construct($testValue)
+    public function __construct(string $testValue)
     {
         $this->testValue = $testValue;
+    }
+
+    public function throwExceptionOnHandle()
+    {
+        $this->throwException = true;
     }
 
     /**
      * @param Request $request
      * @return array|mixed
+     * @throws \Exception
      */
     public function handle(Request $request)
     {
+        if ($this->throwException) {
+            throw new \Exception('some error');
+        }
         return ['value' => $this->testValue];
     }
 }
---
+++ tests/ServerTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace MicroShard\JsonRpcServer\Test;
+
+use MicroShard\JsonRpcServer\Directory;
+use MicroShard\JsonRpcServer\Request;
+use MicroShard\JsonRpcServer\Security\AllowAllAuthenticator;
+use MicroShard\JsonRpcServer\Security\StaticTokenAuthenticator;
+use MicroShard\JsonRpcServer\Server;
+use MicroShard\JsonRpcServer\Test\Mocks\SimpleTestHandler;
+use MicroShard\JsonRpcServer\Test\Mocks\TestHttpRequest;
+
+class ServerTest extends TestCase
+{
+
+    protected $httpResponseCodeMock;
+    protected $httpResponseHeaderMock;
+
+    public function setUp()/* The :void return type declaration that should be here would cause a BC issue */
+    {
+        // mock http_response_code
+        if (is_null($this->httpResponseCodeMock)) {
+            $this->httpResponseCodeMock = $this->mockGlobalMethod('http_response_code', function ($value) {
+            });
+        }
+        $this->httpResponseCodeMock->enable();
+
+        // mock header
+        if (is_null($this->httpResponseHeaderMock)) {
+            $this->httpResponseHeaderMock = $this->mockGlobalMethod('header', function ($value) {
+            });
+        }
+        $this->httpResponseHeaderMock->enable();
+    }
+
+    public function tearDown()/* The :void return type declaration that should be here would cause a BC issue */
+    {
+        $this->httpResponseCodeMock->disable();
+        $this->httpResponseHeaderMock->disable();
+    }
+
+    public function testSuccess()
+    {
+        $authenticator = new AllowAllAuthenticator();
+        $directory = new Directory();
+        $handler = new SimpleTestHandler('test result');
+        $directory->addHandler('resource_a', 'method_1', 1, $handler);
+
+        $httpRequest = new TestHttpRequest();
+        $httpRequest->setBodyData([
+            Request::FIELD_RESOURCE => 'resource_a',
+            Request::FIELD_METHOD => 'method_1',
+        ]);
+
+        $server = new Server($directory, $authenticator);
+
+        $this->expectOutputString('{"status":200,"payload":{"value":"test result"},"resource":"resource_a","method":"method_1","version":"latest","message":"OK"}');
+        $server->run($httpRequest);
+    }
+
+    public function testMalformedRequest()
+    {
+        $authenticator = new AllowAllAuthenticator();
+        $directory = new Directory();
+        $handler = new SimpleTestHandler('test result');
+        $directory->addHandler('resource_a', 'method_1', 1, $handler);
+
+        $httpRequest = new TestHttpRequest();
+        $httpRequest->setBody('{foo');
+
+        $server = new Server($directory, $authenticator);
+
+        $this->expectOutputString('{"status":400,"payload":[],"error":100,"message":"malformed request json"}');
+        $server->run($httpRequest);
+    }
+
+    public function testInvalidAuth()
+    {
+        $authenticator = new StaticTokenAuthenticator('token');
+        $directory = new Directory();
+        $handler = new SimpleTestHandler('test result');
+        $directory->addHandler('resource_a', 'method_1', 1, $handler);
+
+        $httpRequest = new TestHttpRequest();
+        $httpRequest->setBodyData([
+            Request::FIELD_RESOURCE => 'resource_a',
+            Request::FIELD_METHOD => 'method_1',
+        ]);
+
+        $server = new Server($directory, $authenticator);
+
+        $this->expectOutputString('{"status":401,"payload":[],"resource":"resource_a","method":"method_1","version":"latest","error":120,"message":"unauthorized"}');
+        $server->run($httpRequest);
+    }
+
+    public function testException()
+    {
+        $authenticator = new AllowAllAuthenticator();
+        $directory = new Directory();
+        $handler = new SimpleTestHandler('test result');
+        $handler->throwExceptionOnHandle();
+        $directory->addHandler('resource_a', 'method_1', 1, $handler);
+
+        $httpRequest = new TestHttpRequest();
+        $httpRequest->setBodyData([
+            Request::FIELD_RESOURCE => 'resource_a',
+            Request::FIELD_METHOD => 'method_1',
+        ]);
+
+        $server = new Server($directory, $authenticator);
+
+        $this->expectOutputString('{"status":500,"payload":[],"resource":"resource_a","method":"method_1","version":"latest","error":999,"message":"some error"}');
+        $server->run($httpRequest);
+    }
+}
